fb5931ec59aac7cbcbcb567a7b9e828dd34d6693
Chris Yeninas <844685+PhantomGamers@users.noreply.github.com>
2021-08-06 02:44:16 -0400

    revert loading mirror.dll from mod path
    - this seems to just always result in an exception on the first run so let's just include and use our own Mirror.dll
    - change mirrorDllLocation to const

--- Program.cs
+++ Program.cs
@@ -9,10 +9,9 @@ namespace Mirror_External_Weaver
     {
         static void Main(string[] args)
         {
-            mirrorDllLocation = Path.Combine(Path.GetDirectoryName(args[0].Trim('"')), mirrorDllLocation);
             Weaver.WeaveAssembly(Path.GetFullPath(args[0].Trim('"')), new string[] { Path.GetFullPath(args[1].Trim('"')) });
         }
 
-        public static string mirrorDllLocation = "Mirror.dll";
+        public const string mirrorDllLocation = "Mirror.dll";
     }
 }
